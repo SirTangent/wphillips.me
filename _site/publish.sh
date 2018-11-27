d7069cf2c273e9d9c06c1a912c62503e6c4129aa
Wyatt Phillips <datscigye@gmail.com>
2018-11-27 12:00:31 -0500

    27/11/2018 12:00:31

---
+++ _site/publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "compiling..."
+echo ""
+rm -r _site
+jekyll build
+echo ""
+commit=$(date '+%d/%m/%Y %H:%M:%S');
+echo "preparing git commit \"$commit\"..."
+git add .
+git commit -m "$commit"
+echo "pushing to master repo..."
+git push origin master
+echo "push complete! external deployer should be notified."
+
